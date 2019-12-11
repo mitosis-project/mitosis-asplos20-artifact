d98ebf9a9bb928c901a48eba7b83e3b9bfe43935
Panwar, Ashish <panwarit014@gmail.com>
2019-12-11 06:57:49 -0800

    Update figure-9 script
    Signed-off-by: Panwar, Ashish <panwarit014@gmail.com>

--- scripts/run_f9_one.sh
+++ scripts/run_f9_one.sh
@@ -20,6 +20,7 @@
 PERF_EVENTS=cycles,dTLB-loads,dTLB-load-misses,dTLB-stores,dTLB-store-misses,dtlb_load_misses.walk_duration,dtlb_store_misses.walk_duration,page_walker_loads.dtlb_l1,page_walker_loads.dtlb_l2,page_walker_loads.dtlb_l3,page_walker_loads.dtlb_memory,page_walker_loads.dtlb_l1,page_walker_loads.dtlb_l2,page_walker_loads.dtlb_l3,page_walker_loads.dtlb_memory,LLC-loads,LLC-load-misses,LLC-stores,LLC-store-misses
 NR_PTCACHE_PAGES=1100000 # --- 2GB per socket
 XSBENCH_ARGS=" -- -p 25000000 -g 920000 "
+GRAPH500_ARGS=" -- -s 29 -e 21"
 BENCH_ARGS=""
 
 
@@ -170,6 +171,8 @@ test_and_set_configs()
 
         if [ $BENCHMARK == "xsbench" ]; then
                 BENCH_ARGS=$XSBENCH_ARGS
+        elif [ $BENCHMARK == "graph500" ]; then
+                BENCH_ARGS=$GRAPH500_ARGS
         fi
 
 }
@@ -221,6 +224,6 @@ validate_benchmark_config $BENCHMARK $CONFIG
 prepare_benchmark_name $BENCHMARK
 test_and_set_pathnames
 test_and_set_configs $CONFIG
-prepare_datasets
+prepare_datasets $BENCHMARK
 # --- finally, launch the job
 launch_benchmark_config
